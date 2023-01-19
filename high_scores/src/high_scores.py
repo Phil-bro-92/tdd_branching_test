5a32c5b1f952aa1d4bf55c5f2f740643e91f984e
Phil Broadley <philbrocoding@hotmail.com>
2023-01-19 19:55:40 +0000

    Completed high scores feature

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -1,10 +1,35 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    top_three_scores = []
+    remove_duplicates = []
+    [
+        remove_duplicates.append(score)
+        for score in scores
+        if score not in remove_duplicates
+    ]
+
+    remove_duplicates.sort()
+    if len(scores) == 1:
+        top_three_scores.append(remove_duplicates[-1])
+    elif len(scores) < 3:
+        top_three_scores.append(remove_duplicates[-1])
+        top_three_scores.append(remove_duplicates[-2])
+    else:
+        top_three_scores.append(remove_duplicates[-1])
+        top_three_scores.append(remove_duplicates[-2])
+        top_three_scores.append(remove_duplicates[-3])
+    
+    return top_three_scores
+
+
+def sort_high_to_low(scores):
+    scores.sort()
+    scores.reverse()
+    return scores
--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -1,24 +1,38 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import *
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoresTest(unittest.TestCase):
-    pass
-    # Tests
+    def setUp(self):
+        self.scores = [8, 3, 6, 9, 10]
+        self.tie = [3, 4, 7, 7, 1, 2]
+        self.two_scores = [3, 6]
+        self.one_score = [6]
 
     # Test latest score (the last thing in the list)
+    def test_latest_score_is_10(self):
+        self.assertEqual(10, latest(self.scores))
 
     # Test personal best (the highest score in the list)
+    def test_highest_score_is_10(self):
+        self.assertEqual(10, personal_best(self.scores))
 
     # Test top three from list of scores
+    def test_top_three_are_8_9_10(self):
+        self.assertEqual([10, 9, 8], personal_top_three(self.scores))
 
     # Test ordered from highest tp lowest
+    def test_list_ordered_highest_to_lowest(self):
+        self.assertEqual([10, 9, 8, 6, 3], sort_high_to_low(self.scores))
 
     # Test top three when there is a tie
+    def test_top_three_when_tie(self):
+        self.assertEqual([7, 4, 3], personal_top_three(self.tie))
 
     # Test top three when there are less than three
-
+    def test_top_three_when_less_than_three(self):
+        self.assertEqual([6, 3], personal_top_three(self.two_scores))
     # Test top three when there is only one
